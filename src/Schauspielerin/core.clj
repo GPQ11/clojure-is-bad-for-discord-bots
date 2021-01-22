d836eccb6025bb66405a72919e4b04b622cacb16
chrissx <chrissx@chrissx.de>
2021-01-22 12:32:19 +0100

    started writing some code

--- src/Schauspielerin/core.clj
+++ src/Schauspielerin/core.clj
@@ -1,7 +1,18 @@
 (ns Schauspielerin.core
+  (:require [discord.bot :as bot] [discord.http :as http])
   (:gen-class))
 
+(bot/defcommand role
+  [client message]
+  "Adds a role to the user. (doesnt work currently)"
+  (bot/say (http/get-roles client (get-in message :author))))
+
+(bot/defcommand unrole
+  [client message]
+  "Removes a role from the user."
+  (bot/say "https://giphy.com/gifs/9K2nFglCAQClO"))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Starts the bot."
   [& args]
-  (println "Hello, World!"))
+  (bot/start))
