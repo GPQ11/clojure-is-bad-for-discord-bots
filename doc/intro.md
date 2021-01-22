edf53e2d00e96482f38609e5a20f3ed87bd121f6
chrissx <chrissx@chrissx.de>
2021-01-22 10:55:34 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to Schauspielerin
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/Schauspielerin/core_test.clj
@@ -0,0 +1,7 @@
+(ns Schauspielerin.core-test
+  (:require [clojure.test :refer :all]
+            [Schauspielerin.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
